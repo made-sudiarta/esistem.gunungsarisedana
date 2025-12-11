1184755bf446bc8e304a5de82da546c9239e1a9f
Made-sudiarta <>
2025-12-12 03:01:14 +0800

    fix

--- app/Policies/SimpananBerjangkaPolicy.php
+++ app/Policies/SimpananBerjangkaPolicy.php
@@ -16,7 +16,7 @@ class SimpananBerjangkaPolicy
     public function before(User $user, $ability)
     {
         if ($user->hasRole('super_admin')) {
-            return false; // Super Admin bisa melakukan semua aksi
+            return true; // Super Admin bisa melakukan semua aksi
         }
     }
 
