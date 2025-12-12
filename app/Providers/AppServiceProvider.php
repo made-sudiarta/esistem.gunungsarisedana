92ba38eb34640640623cae95fd5380f32ef0bfd6
Made-sudiarta <>
2025-12-12 11:35:14 +0800

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
         TrxSukarela::observe(TrxSukarelaObserver::class);
         
     }
