f832bfc1940507274179f6e64f78dde408144cf6
Made-sudiarta <>
2025-12-29 19:05:16 +0800

    Update-Auth-UI

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -77,38 +77,35 @@ class AdminPanelProvider extends PanelProvider
             ->renderHook(
                 'panels::head.end',
                 fn () => <<<'HTML'
-            <style>
-            /* Force 50/50 split */
-            .auth-ui-enhancer-wrapper {
-                display: flex !important;
-            }
-            .custom-auth-empty-panel{
-                width: 100% !important;
-            }
+                <style>
+                    .auth-ui-enhancer-wrapper {
+                        display: flex !important;
+                    }
+                    .custom-auth-empty-panel{
+                        width: 100% !important;
+                    }
 
-            .auth-ui-enhancer-form-panel {
-                flex: 0 0 50% !important;
-                max-width: 50% !important;
-            }
+                    .auth-ui-enhancer-form-panel {
+                        flex: 0 0 50% !important;
+                        max-width: 50% !important;
+                    }
 
-            .auth-ui-enhancer-empty-panel {
-                flex: 0 0 50% !important;
-                max-width: 50% !important;
-            }
+                    .auth-ui-enhancer-empty-panel {
+                        flex: 0 0 50% !important;
+                        max-width: 50% !important;
+                    }
 
-            /* Mobile */
-            @media (max-width: 1024px) {
-                .auth-ui-enhancer-form-panel,
-                .auth-ui-enhancer-empty-panel {
-                    flex: 0 0 100% !important;
-                    max-width: 100% !important;
-                }
-            }
-            </style>
-            HTML
+                    /* Mobile */
+                    @media (max-width: 1024px) {
+                        .auth-ui-enhancer-form-panel,
+                        .auth-ui-enhancer-empty-panel {
+                            flex: 0 0 100% !important;
+                            max-width: 100% !important;
+                        }
+                    }
+                </style>
+                HTML
             )
-
-
             ->plugins([
                 \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make(),
                 AuthUIEnhancerPlugin::make()
