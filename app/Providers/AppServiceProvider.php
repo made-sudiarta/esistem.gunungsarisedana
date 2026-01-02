fb2cf58d747be976a9e0e02f0ca928f2382c842e
Made-sudiarta <>
2026-01-03 01:51:08 +0800

    back-url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // if (config('app.env') !== 'local') {
-        //     URL::forceScheme('https');
-        // }
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
         TrxSukarela::observe(TrxSukarelaObserver::class);
 
         // Date::setLocale('id');
