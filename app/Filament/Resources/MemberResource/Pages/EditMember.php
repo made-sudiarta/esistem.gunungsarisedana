3a7aadc6e2779de1e589c2571a2cff585f222660
Made-sudiarta <>
2026-03-12 15:02:42 +0800

    update-member-auto-index

--- app/Filament/Resources/MemberResource/Pages/EditMember.php
+++ app/Filament/Resources/MemberResource/Pages/EditMember.php
@@ -18,4 +18,8 @@ class EditMember extends EditRecord
             Actions\RestoreAction::make(),
         ];
     }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
