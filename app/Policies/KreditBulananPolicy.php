bf11c6b145a2948348a31e4dd993249eca3eced3
Made-sudiarta <>
2026-04-27 01:47:35 +0800

    update-policy

--- app/Policies/KreditBulananPolicy.php
+++ app/Policies/KreditBulananPolicy.php
@@ -3,10 +3,10 @@
 namespace App\Policies;
 
 use App\Models\User;
-use App\Models\Absensi;
+use App\Models\KreditBulanan;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class AbsensiPolicy
+class KreditBulananPolicy
 {
     use HandlesAuthorization;
 
@@ -15,15 +15,15 @@ class AbsensiPolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->can('view_any_absensi');
+        return $user->can('view_any_kredit::bulanan');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Absensi $absensi): bool
+    public function view(User $user, KreditBulanan $kreditBulanan): bool
     {
-        return $user->can('view_absensi');
+        return $user->can('view_kredit::bulanan');
     }
 
     /**
@@ -31,23 +31,23 @@ class AbsensiPolicy
      */
     public function create(User $user): bool
     {
-        return $user->can('create_absensi');
+        return $user->can('create_kredit::bulanan');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Absensi $absensi): bool
+    public function update(User $user, KreditBulanan $kreditBulanan): bool
     {
-        return $user->can('update_absensi');
+        return $user->can('update_kredit::bulanan');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Absensi $absensi): bool
+    public function delete(User $user, KreditBulanan $kreditBulanan): bool
     {
-        return $user->can('delete_absensi');
+        return $user->can('delete_kredit::bulanan');
     }
 
     /**
@@ -55,15 +55,15 @@ class AbsensiPolicy
      */
     public function deleteAny(User $user): bool
     {
-        return $user->can('delete_any_absensi');
+        return $user->can('delete_any_kredit::bulanan');
     }
 
     /**
      * Determine whether the user can permanently delete.
      */
-    public function forceDelete(User $user, Absensi $absensi): bool
+    public function forceDelete(User $user, KreditBulanan $kreditBulanan): bool
     {
-        return $user->can('force_delete_absensi');
+        return $user->can('force_delete_kredit::bulanan');
     }
 
     /**
@@ -71,15 +71,15 @@ class AbsensiPolicy
      */
     public function forceDeleteAny(User $user): bool
     {
-        return $user->can('force_delete_any_absensi');
+        return $user->can('force_delete_any_kredit::bulanan');
     }
 
     /**
      * Determine whether the user can restore.
      */
-    public function restore(User $user, Absensi $absensi): bool
+    public function restore(User $user, KreditBulanan $kreditBulanan): bool
     {
-        return $user->can('restore_absensi');
+        return $user->can('restore_kredit::bulanan');
     }
 
     /**
@@ -87,15 +87,15 @@ class AbsensiPolicy
      */
     public function restoreAny(User $user): bool
     {
-        return $user->can('restore_any_absensi');
+        return $user->can('restore_any_kredit::bulanan');
     }
 
     /**
      * Determine whether the user can replicate.
      */
-    public function replicate(User $user, Absensi $absensi): bool
+    public function replicate(User $user, KreditBulanan $kreditBulanan): bool
     {
-        return $user->can('replicate_absensi');
+        return $user->can('replicate_kredit::bulanan');
     }
 
     /**
@@ -103,6 +103,6 @@ class AbsensiPolicy
      */
     public function reorder(User $user): bool
     {
-        return $user->can('reorder_absensi');
+        return $user->can('reorder_kredit::bulanan');
     }
 }
--- app/Policies/SuratTagihanKreditPolicy.php
+++ app/Policies/SuratTagihanKreditPolicy.php
@@ -3,10 +3,10 @@
 namespace App\Policies;
 
 use App\Models\User;
-use App\Models\Absensi;
+use App\Models\SuratTagihanKredit;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class AbsensiPolicy
+class SuratTagihanKreditPolicy
 {
     use HandlesAuthorization;
 
@@ -15,15 +15,15 @@ class AbsensiPolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->can('view_any_absensi');
+        return $user->can('view_any_surat::tagihan::kredit');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Absensi $absensi): bool
+    public function view(User $user, SuratTagihanKredit $suratTagihanKredit): bool
     {
-        return $user->can('view_absensi');
+        return $user->can('view_surat::tagihan::kredit');
     }
 
     /**
@@ -31,23 +31,23 @@ class AbsensiPolicy
      */
     public function create(User $user): bool
     {
-        return $user->can('create_absensi');
+        return $user->can('create_surat::tagihan::kredit');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Absensi $absensi): bool
+    public function update(User $user, SuratTagihanKredit $suratTagihanKredit): bool
     {
-        return $user->can('update_absensi');
+        return $user->can('update_surat::tagihan::kredit');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Absensi $absensi): bool
+    public function delete(User $user, SuratTagihanKredit $suratTagihanKredit): bool
     {
-        return $user->can('delete_absensi');
+        return $user->can('delete_surat::tagihan::kredit');
     }
 
     /**
@@ -55,15 +55,15 @@ class AbsensiPolicy
      */
     public function deleteAny(User $user): bool
     {
-        return $user->can('delete_any_absensi');
+        return $user->can('delete_any_surat::tagihan::kredit');
     }
 
     /**
      * Determine whether the user can permanently delete.
      */
-    public function forceDelete(User $user, Absensi $absensi): bool
+    public function forceDelete(User $user, SuratTagihanKredit $suratTagihanKredit): bool
     {
-        return $user->can('force_delete_absensi');
+        return $user->can('force_delete_surat::tagihan::kredit');
     }
 
     /**
@@ -71,15 +71,15 @@ class AbsensiPolicy
      */
     public function forceDeleteAny(User $user): bool
     {
-        return $user->can('force_delete_any_absensi');
+        return $user->can('force_delete_any_surat::tagihan::kredit');
     }
 
     /**
      * Determine whether the user can restore.
      */
-    public function restore(User $user, Absensi $absensi): bool
+    public function restore(User $user, SuratTagihanKredit $suratTagihanKredit): bool
     {
-        return $user->can('restore_absensi');
+        return $user->can('restore_surat::tagihan::kredit');
     }
 
     /**
@@ -87,15 +87,15 @@ class AbsensiPolicy
      */
     public function restoreAny(User $user): bool
     {
-        return $user->can('restore_any_absensi');
+        return $user->can('restore_any_surat::tagihan::kredit');
     }
 
     /**
      * Determine whether the user can replicate.
      */
-    public function replicate(User $user, Absensi $absensi): bool
+    public function replicate(User $user, SuratTagihanKredit $suratTagihanKredit): bool
     {
-        return $user->can('replicate_absensi');
+        return $user->can('replicate_surat::tagihan::kredit');
     }
 
     /**
@@ -103,6 +103,6 @@ class AbsensiPolicy
      */
     public function reorder(User $user): bool
     {
-        return $user->can('reorder_absensi');
+        return $user->can('reorder_surat::tagihan::kredit');
     }
 }
